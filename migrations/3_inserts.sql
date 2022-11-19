d06865513233e0a8344dd3f4f54263d5c7aad969
darkarp <mario@whitehathacking.tech>
2022-11-19 20:22:25 +0000

    Co-authored-by: Maria Martins <mariamartins03@users.noreply.github.com>

---
+++ migrations/3_inserts.sql
@@ -0,0 +1,4 @@
+-- Add users
+insert into users (usr_name, usr_email, usr_password_hash, usr_role) values('Francisco', 'francisco@gmail.com', 'test123', 'user');
+insert into users (usr_name, usr_email, usr_password_hash, usr_role) values('Maria', 'maria@gmail.com', 'test123', 'admin');
+insert into users (usr_name, usr_email, usr_password_hash, usr_role) values('Mario', 'mario@gmail.com', 'test123', 'user');
